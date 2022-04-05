d9e4050eda0f9dfbb4b44a64a4c890580d606565
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-05 22:17:09 +0900

    Initial Commit

---
+++ NatureUp/NatureUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureUpApp.swift
+//  NatureUp
+//
+//  Created by Wonhyuk Choi on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct NatureUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
