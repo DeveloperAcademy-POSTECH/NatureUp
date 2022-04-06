97609a9fcd731fd70e025258b32c58cedec2d785
Jung Yunseong <cysseo1@naver.com>
2022-04-06 14:44:29 +0900

    기능추가 : 인증화면 추가

---
+++ NatureUp/AuthenticationScreen.swift
@@ -0,0 +1,21 @@
+//
+//  AuthenticationScreen.swift
+//  NatureUp
+//
+//  Created by Jung Yunseong on 2022/04/05.
+//
+
+import SwiftUI
+
+struct AuthenticationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("활동 인증하기", displayMode:.automatic)
+    }
+}
+
+struct AuthenticationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationScreen()
+    }
+}
