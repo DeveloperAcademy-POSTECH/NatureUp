4452f091663087e42ef91ab2b3c0482cd54e5045
Gold-MineScore <96890040+Gold-MineScore@users.noreply.github.com>
2022-04-07 17:33:45 +0900

    상점 아이템 목록 임시 추가

---
+++ NatureUp/Views/Market/ItemList3.swift
@@ -0,0 +1,20 @@
+//
+//  ItemList3.swift
+//  NatureUp
+//
+//  Created by HWANG-C-K on 2022/04/07.
+//
+
+import SwiftUI
+
+struct ItemList3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemList3_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemList3()
+    }
+}
--- NatureUp/Views/Market/MarketView.swift
+++ NatureUp/Views/Market/MarketView.swift
@@ -12,37 +12,51 @@ struct MarketView: View {
     @State private var pickerColer = Color(.sRGB, red: 0.2510, green: 0.7804, blue: 0.5217)
     
     var body: some View {
-        VStack {
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.white)
-                    .border(Color.green, width:3)
-                    .frame(width:135, height:40)
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.white)
-                    .border(Color.green, width:3)
-                    .frame(width:135, height:40)
-            }.padding()
-            RoundedRectangle(cornerRadius : 10)
-                .fill(Color.green)
-                .frame(width:280, height:80)
-            Picker(selection: $selectedNum, label: Text("Item List")) {
-                Text("테두리")
-                    .tag(1)
-                Text("보석 장식")
-                    .tag(2)
-                Text("리본")
-                    .tag(3)
-                Text("기타")
-                    .tag(4)
-            }.pickerStyle(SegmentedPickerStyle())
-                .padding()
-            if (selectedNum == 1){
-                ItemList1()
+        NavigationView{
+            ZStack{
+                Color("BackgroundGray")
+                    .navigationBarTitle("상점", displayMode: .inline)
+                VStack {
+                    HStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .border(Color.green, width:3)
+                            .frame(width:135, height:40)
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .border(Color.green, width:3)
+                            .frame(width:135, height:40)
+                    }.padding()
+                    RoundedRectangle(cornerRadius : 10)
+                        .fill(Color("SecondGreen"))
+                        .frame(width:280, height:80)
+                    Picker(selection: $selectedNum, label:      Text("Item List")) {
+                        Text("테두리")
+                            .tag(1)
+                        Text("보석 장식")
+                            .tag(2)
+                        Text("리본")
+                            .tag(3)
+                        Text("기타")
+                            .tag(4)
+                    }.pickerStyle   (SegmentedPickerStyle())
+                        .padding()
+                    if (selectedNum == 1){
+                        ItemList1()
+                    }
+                    else if (selectedNum == 2){
+                        ItemList2()
+                    }
+                    else if (selectedNum == 3){
+                        ItemList3()
+                    }
+                    else if (selectedNum == 4){
+                        ItemList4()
+                    }
+                    Spacer()
+                }
             }
-            Spacer()
         }
-
     }
 }
 
