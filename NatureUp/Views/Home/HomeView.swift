f75d4dcc5c3a5231f4e1f357daba9c4ec8735922
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-07 17:24:25 +0900

    기능추가 : 홈 화면 완성
    홈 화면에 표시된 활동 리스트, 오차 주의 사항, 버튼 구현하였습니다.

--- NatureUp/Views/Home/HomeView.swift
+++ NatureUp/Views/Home/HomeView.swift
@@ -2,7 +2,7 @@
 //  HomeView.swift
 //  NatureUp
 //
-//  Created by Wonhyuk Choi on 2022/04/06.
+//  Created by Wonhyuk Choi on 2022/04/07.
 //
 
 import SwiftUI
@@ -14,17 +14,47 @@ struct HomeView: View {
         NavigationView{
             ZStack {
                 Color("BackgroundGray")
-                MyStatus()
-                    .frame(height: 80)
-                    .navigationBarTitle("Home", displayMode: .inline)
-                    .toolbar {
-                        Button {
-                            showingNotifications.toggle()
-                        } label: {
-                            Label("Notifications", systemImage: "bell.badge")
-                        }
-                        .accentColor(Color("PrimaryGreen"))
+                VStack {
+                    MyStatus()
+                        .frame(height: 80)
+                        .navigationBarTitle("Home", displayMode: .inline)
+                        .toolbar {
+                            Button {
+                                showingNotifications.toggle()
+                            } label: {
+                                Label("Notifications", systemImage: "bell.badge")
+                            }
+                            .accentColor(Color("PrimaryGreen"))
                     }
+                    ScrollView {
+                        ActivityList(location: "당신이 있는 건물 내")
+                        ActivityList(location: "창문 밖으로 보이는 바로 그 곳")
+                        HStack {
+                            Spacer()
+                            Text("반경 20m내 오차범위가 있을 수 있습니다.")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                                .foregroundColor(Color.gray)
+                                .padding()
+                        }
+                    }
+                    Button(action: {
+                            print("sign up bin tapped")
+                    }) {
+                        Text("랜덤평가 진행하기")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .font(.system(size: 18))
+                            .padding()
+                            .foregroundColor(.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("PrimaryGreen"), lineWidth: 2)
+                            )
+                    }
+                    .background(Color("SecondGreen"))
+                    .cornerRadius(10)
+                    .padding()
+                }
             }
         }
     }
