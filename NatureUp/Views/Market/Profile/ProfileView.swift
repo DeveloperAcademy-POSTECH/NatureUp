bc55453700b897fee8b1928cd1f393c283ca5c8b
pig1606 <pig1606@knu.ac.kr>
2022-04-08 15:59:30 +0900

    형식변경: 배경색추가
    에버렛 사진 추가하기전에 배경 회색으로 바꾼건데 커밋을 안했군요 지금 하겠습니다

---
+++ NatureUp/Views/Market/Profile/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  NatureUp
+//
+//  Created by Minyoung Kim on 2022/04/08.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+
+        ZStack {
+            Color("BackgroundGray")
+            ProfilePicture()
+                .padding(.bottom, 400)
+            
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
