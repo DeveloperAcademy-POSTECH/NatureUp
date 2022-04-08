eb96b1bd739ffff4d7717cdd1c56b88136277c22
JungYunseong <cysseo1@naver.com>
2022-04-09 03:06:45 +0900

    기능 수정 : 인증페이지 수정
    인증페이지 컴포넌트 추가

--- NatureUp/Views/Evaluation/EvaluationView.swift
+++ NatureUp/Views/Evaluation/EvaluationView.swift
@@ -9,7 +9,66 @@ import SwiftUI
 
 struct EvaluationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundGray")
+            VStack {
+                Text("다른 사람 평가하기")
+                    .font(.title)
+                    .padding(1)
+                Text("활동에 맞는 올바른 사진인지 평가해주세요")
+                    .font(.body)
+                    .padding(.bottom)
+                HStack {
+                    ProgressView(value: 0.1)
+                    Spacer()
+                }
+                .padding()
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("PrimaryGreen"), lineWidth: 1)
+                        )
+                    
+                    Text("쓰레기 분리수거하기")
+                        .font(.title2)
+                        .foregroundColor(Color("PrimaryGreen"))
+                }
+                Rectangle()
+                    .frame(height: 400)
+                    .cornerRadius(10)
+                HStack {
+                    Button(action: {}, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(.white)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .cornerRadius(10)
+                            Image(systemName: "xmark")
+                                .foregroundColor(.black)
+                                .font(.system(size: 30))
+                        }
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color(red: 102.0 / 255, green: 210.0 / 255, blue: 157.0 / 255))
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .cornerRadius(10)
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.white)
+                                .font(.system(size: 30))
+                        }
+                    })
+                }
+            }
+            .padding()
+        }
+        .navigationBarTitle("활동 인증하기", displayMode:.inline)
+        .accentColor(Color("PrimaryGreen"))
     }
 }
 
