e491447a2d0333257a1389d95754bcd9119c0e71
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-09 23:35:34 +0900

    기능수정 : NavigationView 를 탭뷰 바깥으로 이동
    NavigationView를 탭 뷰 밖으로 이동함으로써 네비게이션할 때 탭뷰가 유지되는 버그를 수정하였습니다.

--- NatureUp/Views/LeaderBoard/LeaderBoardView.swift
+++ NatureUp/Views/LeaderBoard/LeaderBoardView.swift
@@ -12,46 +12,77 @@ struct LeaderBoardView: View {
     let no: String  = "10"
     let point: String = "100"
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color("BackgroundGray")
-                    .navigationBarTitle("리더 보드", displayMode: .inline)
-                ScrollView {
+        ZStack {
+            Color("BackgroundGray")
+            ScrollView {
+                VStack(spacing: -10) {
+                    HStack {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("SecondGreen"), lineWidth: 2)
+                                .frame(width:150, height:40)
+                            RoundedRectangle(cornerRadius:10)
+                                .fill(Color("SecondGreen"))
+                                .opacity(0.1)
+                                .frame(width:150, height:40)
+                            Text("내등수 :  \(no)")
+                        }
+                        Spacer()
+                            .frame(width:30)
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("SecondGreen"), lineWidth: 2)
+                                .frame(width:150, height:40)
+                            RoundedRectangle(cornerRadius:10)
+                                .fill(Color("SecondGreen"))
+                                .opacity(0.1)
+                                .frame(width:150, height:40)
+                            Text("포인트 : \(point)")
+                        }
+                    }.padding()
                     
-                    VStack(spacing: -10) {
-                        HStack {
-                            ZStack{
-                                RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color("SecondGreen"), lineWidth: 2)
-                                            .frame(width:150, height:40)
-                                RoundedRectangle(cornerRadius:10)
-                                    .fill(Color("SecondGreen"))
-                                    .opacity(0.1)
-                                    .frame(width:150, height:40)
-                                Text("내등수 :  \(no)")
-                            }
-                            Spacer()
-                                .frame(width:30)
-                            ZStack{
-                                RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color("SecondGreen"), lineWidth: 2)
-                                            .frame(width:150, height:40)
-                                RoundedRectangle(cornerRadius:10)
-                                    .fill(Color("SecondGreen"))
-                                    .opacity(0.1)
-                                    .frame(width:150, height:40)
-                                Text("포인트 : \(point)")
-                            }
-                        }.padding()
-                        
-                        VStack(spacing: -20) {
-                            VStack {
-                                Image("no1")
-                                    .resizable()
+                    VStack(spacing: -20) {
+                        VStack {
+                            Image("no1")
+                                .resizable()
                                 .frame(width: 40, height: 40, alignment: .center)
+                            Image("user1")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(Circle())
+                                .shadow(radius: 10)
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 7))
+                            Image("userName1")
+                                .resizable()
+                                .frame(width: 150, height: 40, alignment: .center)
+                        }
+                        
+                        
+                        HStack() {
+                            VStack {
+                                Image("no2")
+                                    .resizable()
+                                    .frame(width: 40, height: 40, alignment: .center)
                                 Image("user1")
                                     .resizable()
-                                    .frame(width: 150, height: 150)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                                    .shadow(radius: 10)
+                                    .overlay(Circle().stroke(Color.gray, lineWidth: 7))
+                                Image("userName1")
+                                    .resizable()
+                                    .frame(width: 150, height: 40, alignment: .center)
+                            }
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Image("no3")
+                                    .resizable()
+                                    .frame(width: 40, height: 40, alignment: .center)
+                                Image("user1")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
                                     .clipShape(Circle())
                                     .shadow(radius: 10)
                                     .overlay(Circle().stroke(Color.gray, lineWidth: 7))
@@ -61,46 +92,9 @@ struct LeaderBoardView: View {
                             }
                             
                             
-                            HStack() {
-                                VStack {
-                                    Image("no2")
-                                        .resizable()
-                                        .frame(width: 40, height: 40, alignment: .center)
-                                    Image("user1")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .clipShape(Circle())
-                                        .shadow(radius: 10)
-                                        .overlay(Circle().stroke(Color.gray, lineWidth: 7))
-                                    Image("userName1")
-                                        .resizable()
-                                        .frame(width: 150, height: 40, alignment: .center)
-                                }
-                                
-                                Spacer()
-                                
-                                VStack {
-                                    Image("no3")
-                                        .resizable()
-                                        .frame(width: 40, height: 40, alignment: .center)
-                                    Image("user1")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .clipShape(Circle())
-                                        .shadow(radius: 10)
-                                        .overlay(Circle().stroke(Color.gray, lineWidth: 7))
-                                    Image("userName1")
-                                        .resizable()
-                                        .frame(width: 150, height: 40, alignment: .center)
-                                }
-                                
-                                
-                            }
-                            .padding()
-                            
-                            LeaderBoardRow()
                         }
-                        
+                        .padding()
+                        LeaderBoardRow()
                     }
                 }
             }
