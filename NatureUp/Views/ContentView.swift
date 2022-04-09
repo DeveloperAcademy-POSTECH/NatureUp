22f9c51d42b4652464c8b4c707b757ba5d265fed
JungYunseong <cysseo1@naver.com>
2022-04-10 01:07:04 +0900

    버그수정 : 컨텐트뷰 수정
    컨텐트뷰의 네비게이션뷰에 적용되지 않았던 accentColor을 추가 하였습니다

--- NatureUp/Views/ContentView.swift
+++ NatureUp/Views/ContentView.swift
@@ -77,6 +77,7 @@ struct ContentView: View {
             .accentColor(Color("PrimaryGreen"))
             .navigationBarTitle(returnNaviBarTitle(tabSelection: self.tabSelection), displayMode: .inline)
         }
+        .accentColor(Color("PrimaryGreen"))
     }
 }
 
