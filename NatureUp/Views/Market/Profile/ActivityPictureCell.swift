d70c34772d7e473d9f294bf1be469c2d954f5969
pig1606 <pig1606@knu.ac.kr>
2022-04-10 13:25:18 +0900

    기능변경: CollectionView를 도형이 아닌 사진으로 변경
    - CollectionView에서 원래 RoundedRectangle + Text 였던것을 image로 변경했습니다
    - 오해의 소지가 있어 assets의 사진 이름을 1 --> img01 식으로 변경했습니다

--- NatureUp/Views/Market/Profile/ActivityPictureCell.swift
+++ NatureUp/Views/Market/Profile/ActivityPictureCell.swift
@@ -1,5 +1,5 @@
 //
-//  ActivityPictures.swift
+//  ActivityPictureCell.swift
 //  NatureUp
 //
 //  Created by Minyoung Kim on 2022/04/10.
@@ -8,20 +8,18 @@
 import SwiftUI
 
 struct ActivityPictureCell: View {
+    
     static let coulmn = 3
     static let row = 7
     let width =
     (UIScreen.main.bounds.width/3)-25
     
     var body: some View {
-        ZStack{
-        RoundedRectangle(cornerRadius: 8)
-                .frame(width: width, height: width)
-                .foregroundColor(Color.gray)
-        Text("사진")
-                .font(.title3)
-                .foregroundColor(.white)
-        }
+        Image("img05")
+            .resizable()
+            .frame(width: 99, height: 99)
+            .scaledToFill()
+            .clipShape(RoundedRectangle(cornerRadius: 8))
     }
 }
 
