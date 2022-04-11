28305cae376a1850cd7639645fe41ad895ea6404
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-11 14:34:52 +0900

    리팩토링 : Activity 뷰 NavigationView 제거
    Activity 뷰 요소가 NavigationView로 감싸져 있는것을 제거하였습니다.

--- NatureUp/Views/Home/Activity.swift
+++ NatureUp/Views/Home/Activity.swift
@@ -8,32 +8,26 @@
 import SwiftUI
 
 struct Activity: View {
-    @State var isLinkActive : Bool = false
     let activity: String
     
     var body: some View {
-        NavigationLink(destination: AuthenticationView(rootIsActive: $isLinkActive), isActive: $isLinkActive) {
-            Button(action: {self.isLinkActive = true}, label: {
-            ZStack {
-                Color(.white)
-                HStack {
-                    Text(activity)
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundColor(Color.black)
-                    Spacer()
-                }
-                .padding()
+        ZStack {
+            Color(.white)
+            HStack {
+                Text(activity)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.black)
+                Spacer()
             }
-            .cornerRadius(1)
-            .overlay(
-                RoundedRectangle(cornerRadius: 1)
-                    .stroke(Color(red: 151.0/255, green: 151.0/255, blue: 151.0/255), lineWidth: 0.4)
-                )
-            }
-            )
-            .frame(height: 50)
+            .padding()
         }
+        .cornerRadius(1)
+        .overlay(
+            RoundedRectangle(cornerRadius: 1)
+                .stroke(Color(red: 151.0/255, green: 151.0/255, blue: 151.0/255), lineWidth: 0.4)
+        )
+        .frame(height: 50)
     }
 }
 
