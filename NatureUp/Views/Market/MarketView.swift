b73182acf0a1488f31c0045d35fbbfabb248716a
Gold-MineScore <96890040+Gold-MineScore@users.noreply.github.com>
2022-04-11 20:52:27 +0900

    기능추가 : 아이템 목록 아이템 분류 및 사진 추가
     - 아이템 목록 항목 수정
     - 아이템 목록 코드 변경
     - 아이템 목록 json 파일에 분류 추가

--- NatureUp/Views/Market/MarketView.swift
+++ NatureUp/Views/Market/MarketView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MarketView: View {
+    @State private var ItemType = "border"
     @State private var selectedNum = 1
     @State private var pickerColer = Color(.sRGB, red: 0.2510, green: 0.7804, blue: 0.5217)
     
@@ -54,25 +55,25 @@ struct MarketView: View {
                 Picker(selection: $selectedNum, label: Text("Item List")) {
                     Text("테두리")
                         .tag(1)
-                    Text("보석 장식")
+                    Text("장식")
                         .tag(2)
-                    Text("리본")
+                    Text("배경")
                         .tag(3)
-                    Text("기타")
+                    Text("효과")
                         .tag(4)
                 }.pickerStyle   (SegmentedPickerStyle())
                     .padding()
                 if (selectedNum == 1){
-                    ItemCardList()
+                    ItemCardList(ItemType: "border")
                 }
                 else if (selectedNum == 2){
-                    ItemList2()
+                    ItemCardList(ItemType:"decoration")
                 }
                 else if (selectedNum == 3){
-                    ItemList2()
+                    ItemCardList(ItemType:"background")
                 }
                 else if (selectedNum == 4){
-                    ItemList2()
+                    ItemCardList(ItemType: "effect")
                 }
                 Spacer()
             }
