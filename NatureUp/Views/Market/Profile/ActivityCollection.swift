0a05c6a7a5de8f793dde4a544aa7e521fac1d567
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-12 15:55:25 +0900

    프로젝트 : 폴더 경로 수정
    잘못 배치되어있는 폴더 및 파일 재배치하였습니다.

--- NatureUp/Views/Market/Profile/ActivityCollection.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ActivityCollection.swift
-//  NatureUp
-//
-//  Created by Minyoung Kim on 2022/04/08.
-//
-
--- NatureUp/Views/Profile/ProfileView.swift
+++ NatureUp/Views/Profile/ProfileView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @Environment(\.editMode) var editMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundGray")
+            VStack {
+
+                ProfileStatus()
+                ProfilePicture()
+                NamePlate()
+                
+                Text("오늘 지구를 살린 활동")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 35)
+                    .padding(.top, 30)
+                    .padding(.bottom, -3)
+                
+                ActivityAlbum()
+                Spacer()
+                
+            }
+        }
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+        
     }
 }
