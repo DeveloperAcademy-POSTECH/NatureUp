c6ead4c6b0e87284736a063e8c2154ae1e0d9c0e
JungYunseong <cysseo1@naver.com>
2022-04-13 05:52:08 +0900

    버그수정 : 프로필뷰 수정
    프로필 status 파일 수정

--- NatureUp/Views/Market/ItemCardView.swift
+++ NatureUp/Views/Market/ItemCardView.swift
@@ -11,8 +11,8 @@ struct ItemCardView: View {
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("SecondGreen"), lineWidth: 1)
-                        .frame(width:100, height:100)
+                .stroke(Color("SecondGreen"), lineWidth: 1)
+                .frame(width:100, height:100)
             RoundedRectangle(cornerRadius:10)
                 .fill(Color.white)
                 .frame(width:100, height:100)
