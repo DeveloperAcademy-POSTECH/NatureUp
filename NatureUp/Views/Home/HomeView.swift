50e50f212785e8cb5f078add7a2a96b2cae77a95
Wonhyuk Choi <whchoi000@gmail.com>
2022-04-14 13:39:18 +0900

    형식변경 : TODO 주석 추가
    HomeView에서 추가해야할 기능을 TODO 주석으로 작성하였습니다.

--- NatureUp/Views/Home/HomeView.swift
+++ NatureUp/Views/Home/HomeView.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct HomeView: View {    
+struct HomeView: View {
+    
+    // TODO: 1. 자기위치 가져오기
+    // TODO: 2. 자기위치와 가장 가까운 핀 2개 찾기
+    // TODO: 3. 가까운 핀 2개의 타입이 서로 다르면 출력
+    // TODO: 4. 같으면 다를때까지 찾고 출력
+    
     var body: some View {
         ZStack {
             Color("BackgroundGray")
